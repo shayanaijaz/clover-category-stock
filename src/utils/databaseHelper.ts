c7b4ba36dec44503d8353546e0c4488176621baf
shayanaijaz@gmail.com <shayanaijaz@gmail.com>
2023-10-14 19:59:56 -0500

    database variable fix

--- src/utils/databaseHelper.ts
+++ src/utils/databaseHelper.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
